12cdf25091ec1d48be55d17ff517871c411c9982
hunglongtrangithub <hunglongtran2004@gmail.com>
2024-09-15 19:07:44 -0400

    build(vite): target modern browsers' JS

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import glsl from "vite-plugin-glsl";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
+    build: {
+      target: "esnext"
+    },
     plugins: [react(), glsl()],
     server: {
       open: mode === "test" ? "/test/index.html" : "/index.html",
